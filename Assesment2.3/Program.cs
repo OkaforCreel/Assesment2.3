e69e78d97415cb8280fb723dc05860a2f4d2ea13
Creel Okafor <okaforcreel@gmail.com>
2023-04-05 16:58:26 +0100

    Add project files.

---
+++ Assesment2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "foobar";
+        char letter = 'o';
+
+        int count = 0;
+        foreach (char c in s)
+        {
+            if (c == letter)
+            {
+                count++;
+            }
+        }
+
+        double percent = (double)count / s.Length * 100;
+        int roundedPercent = (int)Math.Floor(percent);
+
+        Console.WriteLine(roundedPercent);
+    }
+}
